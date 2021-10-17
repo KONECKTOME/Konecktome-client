15a78bcb81b53b3f0c051b3c775a67572ea2f4a6
QOA <58850207+QAKANDE@users.noreply.github.com>
2021-10-17 19:49:07 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import ComingSoon from "./Components/Comingsoon";
-import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
 import Coming_soon_home from "./Components/Coming_soon_home";
 import Success from "./Components/Success";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  const [firstName, setFirstName] = useState(0);
   return (
     <div className="App">
       <Router>
