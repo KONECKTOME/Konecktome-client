c9efbcfea6171ce6703e446876fda64b8535fcf6
QOA <58850207+QAKANDE@users.noreply.github.com>
2022-06-19 15:23:13 +0100

    push

--- src/Components/Client/Settings/SettingsModal.jsx
+++ src/Components/Client/Settings/SettingsModal.jsx
@@ -21,16 +21,21 @@ class SettingsModal extends React.Component {
             <div id="modal_close_btn"></div>
             <div id="settings_modal_wrapper">
               <p id="settings_modal_header">Profile settings</p>
-              <p id="settings_modal_subheader">
-                Hey, what's your passcode again ?
-              </p>
+              <p id="settings_modal_subheader">Hey, what's your pin again ?</p>
+
               <div id="settings_modal_form_container">
                 <div id="settings_modal_form_subcontainer">
                   <form id="settings_modal_form">
-                    <input type="text" />
+                    <div id="pin-container">
+                      <input type="text" />
+                      <input type="text" />
+                      <input type="text" />
+                      <input type="text" />
+                    </div>
                   </form>
                 </div>
               </div>
+
               <div
                 id="setting_modal_form_button_wrapper"
                 onClick={() => this.props.hideModal()}
