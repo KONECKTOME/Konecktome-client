5fba58332ae963fe9af23191cd85daf65504e29e
QOA <58850207+QAKANDE@users.noreply.github.com>
2022-11-02 16:05:11 +0000

    push

--- src/Components/Client/LandingPage/USP.jsx
+++ src/Components/Client/LandingPage/USP.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "../../../css/LandingPage/usp.css";
+import "../../../css/LandingPage/usp.CSS";
 import { Row, Col } from "react-bootstrap";
 import Exclusive from "../../../Assets/USP Assets/Exclusive.png";
 import Cancellation from "../../../Assets/USP Assets/Cancellation.png";
