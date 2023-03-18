616aec818a232b1d5f86c210e77511f36b9d14ca
QAKANDE <quadriomofolarinakande@gmail.com>
2023-03-18 19:51:35 +0000

    remove router scroll

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import ScrollToTop from "./Components/Reusable/ScrollToTop";
-
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ScrollToTop />
+    {/* <ScrollToTop /> */}
     <App />
   </React.StrictMode>,
   document.getElementById("root")
